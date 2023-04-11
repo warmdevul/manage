35ca3ecee0da0af5ea86db7118cea96d999d9685
Warm <126745522+warmdevul@users.noreply.github.com>
2023-04-11 12:54:40 +0700

    update1

--- manage23.py
+++ manage23.py
@@ -4,23 +4,23 @@ numerical_order = 1
 table = []
 file = open("manage.txt", mode="r", encoding="utf-8-sig")
 def getInfor():
- global numerical_order
- global table
- students = file.readline()
- while students != "":
-    student = students.split(";")
-    full_name = student[0]
-    grade = student[1] 
-    contact = student[2]
-    location = student[3].strip()
-    table.append([numerical_order, full_name, grade, contact, location])
-    numerical_order += 1
+    global numerical_order
+    global table
     students = file.readline()
+    while students != "":
+        student = students.split(";")
+        full_name = student[0]
+        grade = student[1] 
+        contact = student[2]
+        location = student[3].strip()
+        table.append([numerical_order, full_name, grade, contact, location])
+        numerical_order += 1
+        students = file.readline()
 def getMode():
     print("\n1. Information\n" + "2. Search\n" + "3. Edit\n" + "4. Quit\n")
     mode = int(input("Choose your mode: "))
     if mode == 1:
-        print(tabulate(table, headers=["STT", "Ten", "Lop", "So Dien Thoai", "Que Quan"], tablefmt="fancy_grid"))
+        print("\n", tabulate(table, headers=["STT", "Ten", "Lop", "So Dien Thoai", "Que Quan"], tablefmt="fancy_grid"))
         getMode()
     else:
         pass
